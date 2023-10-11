c53999aa8b1419edf2c73e54901e2e00c89d6593
Matteus de Oliveira Car <matteus.car@gmail.com>
2023-10-11 01:26:43 -0300

    Atualização: script compactado com def de funções

--- frissEquation.py
+++ frissEquation.py
@@ -18,7 +18,7 @@ d = 10           #distancia entre as antenas em metros
 #(transmissora e receptora) e a potência transmitida (tudo em Db)
 L0 = 20*math.log10((4*math.pi*d)/lbda)
 Pr = Gt + Gr - L0 + Pt
-print('fim da equação de Friss')
+
 print('A potência recebida pela receptora é:', round(Pr,3), 'dBm')
 
 #Cálculo da distância dados os parâmetros do link entre as antenas,
@@ -28,30 +28,36 @@ d = (lbda/(4*math.pi))*(10**((1/20)*(L0)))                  #20*math.log10((4*ma
                                                             #math.log10((4*math.pi*d)/lbda) = (1/20)*(L0)
                                                             #(4*math.pi*d)/lbda = 10**((1/20)*(L0))
                                                             #d/lbda = (1/(4*math.pi))*(10**((1/20)*(L0)))
-def calculaPotencia():
+def pathLoss(d, lbda):
+    return 20*math.log10((4*math.pi*d)/lbda)
+
+def distanciaEntreAntenas(lbda, L0):
+    return (lbda/(4*math.pi))*(10**((1/20)*(L0)))
+
+def linkParameters():
     Gt = float(input("Digite o valor do ganho da Antena transmissora: "))
     Gr = float(input("Digite o valor do ganho da Antena receptora: "))
     Pt = float(input("Digite o valor da potência de transmissão em dBm: "))
-    d =  float(input("Digite o valor da distância entre as antenas (em metros): "))
     f =  float(input("Digite a frequência de comunicação (em Hz): "))
+    return [Gt,Gr,Pt,f]
+
+def calculaPotencia():
+    [Gt,Gr,Pt,f] = linkParameters()
+    d =  float(input("Digite o valor da distância entre as antenas (em metros): "))
     lbda = c/f
-    L0 = 20*math.log10((4*math.pi*d)/lbda)
+    L0 = pathLoss(d, lbda)
     Pr = Gt + Gr - L0 + Pt
     print("A potência recebida pela antena receptora é: ", Pr, "dBm")
 
 def calculaDistancia():
-    Gt = float(input("Digite o valor do ganho da Antena transmissora: "))
-    Gr = float(input("Digite o valor do ganho da Antena receptora: "))
-    Pt = float(input("Digite o valor da potência de transmissão em dBm: "))
-    Pr =  float(input("Digite o valor esperado ou medido da potência recebida: "))
-    f =  float(input("Digite a frequência de comunicação (em Hz): "))
+    [Gt,Gr,Pt,f] = linkParameters()
+    Pr = float(input("Digite o valor esperado ou medido da potência recebida: "))
     lbda = c/f
     L0 = Pt - Pr + Gt + Gr
-    d = (lbda/(4*math.pi))*(10**((1/20)*(L0)))
+    d = distanciaEntreAntenas(lbda, L0)
     print('A distância calculada foi', d)
 
-modo = 100
-while modo != 0:
+while True:
     modo = int(input("Digite 1 caso queira calcular a potência recebida, 2 caso queira calcular a distância esperada e 0 para fechar o programa: "))
     match modo:
         case 1:
@@ -60,3 +66,4 @@ while modo != 0:
             calculaDistancia()
         case 0:
             print("Fechando programa")
+            break
